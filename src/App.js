c2e919a190140740d9fa55f434712131f53e7459
TSB <122724396+tsbproject@users.noreply.github.com>
2023-10-06 18:34:31 +0100

    [commit]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 function App() {
   return(
-    <h2>Hello Esusu</h2>
+     <div>
+      <Admin />
+     </div>
   )
 }
 
@@ -9,7 +11,20 @@ function App() {
 const Admin =() =>{
   return(
     <div className="admin">
+      <h3>Admin Area, Welcome Contributor!</h3>
+      <label >Username <input type="text" id="username"
+      placeholder="enter username"></input>
+      </label> 
+
+      <label>Password < input type="text" id="password"
+       placeholder="enter password"  /> 
+       </label>
+
+       <button id="login">Login</button>
       
+
+      
+
 
     </div>
   )
